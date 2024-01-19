d5776179969bc39f1524cf7ec0057ce6bd14f06d
gcruzrios <gcruzr@gmail.com>
2024-01-19 14:03:36 -0600

    tercer release breadcrum

--- app/Breadcrum.tsx
+++ app/Breadcrum.tsx
@@ -1,26 +1,45 @@
+'use client'
+import Link from 'next/link';
 import React from 'react'
 
-const Breadcrum = () => {
+import { usePathname } from 'next/navigation'
 
+interface Props{
+    params:{enlace:string[]};
+    busquedaParametro: {ordenProducto:string}
+}
+
+const Breadcrum = () => {
+	
+	//const location = useLocation();
     const styles = {
        background:"url(assets/img/pictures/breadcrumb.jpg)",
         
     };
-    
+
+	
+    const pathname = usePathname()
   return (
     <div>
         {/* <!-- Start Breadcrumb
 		============================================= --> */}
 		<div className="site-breadcrumb" style={styles}>
 			<div className="container">
+			
 				<div className="site-breadcrumb-wpr">
-					<h2 className="breadcrumb-title">About Us</h2>
+					{/* <h2 className="breadcrumb-title">Page: </h2> */}
 					<ul className="breadcrumb-menu clearfix">
-						<li><a href="index.html">Home</a></li>
-						<li className="active">About Us</li>
+						<li>
+							<Link href="/">Home</Link>
+						</li>
+						
+						<li className="active">{pathname}</li>
+						
 					</ul>
 				</div>
+				
 			</div>
+			
 		</div>
         {/* <!-- End Breadcrumb --> */}
     </div>
